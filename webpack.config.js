b1138ede461f276f990e72e2766c631a35f0e5d7
ZhaoTong <zhaotong@meican.com>
2017-11-30 22:46:37 +0800

    rewrite webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,155 +1,65 @@
 const path = require('path');
-const webpack = require('webpack');
 const WXAppWebpackPlugin = require('wxapp-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-
-// import path from 'path';
-// import webpack from 'webpack'
-// import WXAppWebpackPlugin from 'wxapp-webpack-plugin'
-// import CopyWebpackPlugin from 'copy-webpack-plugin'
-// import CleanWebpackPlugin from 'clean-webpack-plugin'
 
 const NODE_ENV = process.env.NODE_ENV;
-const IS_PRODUCTION = 'production' === NODE_ENV;
-const IS_BETA = 'beta' === NODE_ENV;
-const IS_DEVELOPMENT = 'development' === NODE_ENV;
 
-const relativeFileLoader = (ext = '[ext]') => [
-	{
-		loader: 'file-loader',
-		options: {
-			publicPath: '',
-			useRelativePath: true,
-			name: `[name].${ext}`,
-			emitFile: false,
-		},
-	},
-];
-
-let webpackConfig = {
-  entry: {
-    app: [
-      './src/app.js'
-    ],
-  },
-  output: {
-    filename: '[name].js',
-    publicPath: '/',
-    path: path.join(__dirname, 'dist'),
-  },
-
-  module: {
-    loaders: [{
-        test: /\.(woff|woff2|ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader?name=fonts/[name].[ext]',
-      }, {
-        test: /\.(svg)$/,
-        include: path.join(__dirname, 'src/images/'),
-        exclude: /node_modules/,
-        loader: 'url-loader?limit=16384&name=images/[hash].[ext]',
-      }, {
-        test: /\.(png|jpg|gif|icon|ico)$/,
-        include: path.join(__dirname, 'src'),
-        exclude: /node_modules/,
-        loader: 'url-loader?limit=65536&name=images/[hash].[ext]',
-      }, {
-        test: /\.json$/,
-        include: /src/,
-        use: [{
-          loader: 'file-loader',
+module.exports = {
+ // 引入 `app.js`
+ entry: './src/app.js',
+ output: {
+   filename: '[name].js',
+   // 此处 `dist` 为微信开发者工具引入的开发目录
+   path: path.resolve(__dirname, 'dist'),
+ },
+ plugins: [
+   new WXAppWebpackPlugin(),
+ ],
+ module: {
+   loaders: [{
+      test: /\.json$/,
+      include: /src/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          useRelativePath: true,
+          name: '[name].[ext]',
+        },
+      }],
+    }, {
+      test: /\.(js)$/,
+      include: path.join(__dirname, 'src'),
+      exclude: /node_modules/,
+      loaders: ['babel-loader'],
+    }, {
+     test: /\.wxml$/,
+     include: path.resolve('src/pages'),
+      use: [{
+        loader: 'file-loader',
           options: {
             useRelativePath: true,
-            name: '[name].[ext]',
-          },
-        }],
-      }, {
-        test: /\.(js)$/,
-        include: path.join(__dirname, 'src'),
-        exclude: /node_modules/,
-        loaders: ['babel-loader'],
-      }, {
-        test: /\.wxss$/,
-        include: /src/,
-        use: [{
-            loader: 'file-loader',
+              name: '[name].[ext]',
+            },
+          }, {
+          loader: 'wxml-loader',
             options: {
-                useRelativePath: true,
-                name: '[name].wxss',
+              root: path.resolve('src'),
+              minimize: false,
+            },
+        }],
+    }, {
+      test: /\.wxss$/,
+      include: /src/,
+        use: [{
+          loader: 'file-loader',
+            options: {
+              useRelativePath: true,
+              name: '[name].wxss',
             }
         }],
-      }, {
-        test: /\.wxml$/,
-        include: path.resolve('src/pages'),
-        use: [{
-            loader: 'file-loader',
-            options: {
-                useRelativePath: true,
-                name: '[name].[ext]',
-            },
-        }, {
-            loader: 'wxml-loader',
-            options: {
-                root: path.resolve('src'),
-                minimize: false,
-            },
-        }],
-      }, {
-        test: /\.scss$/,
-        include: /src/,
-        use: [
-          ...relativeFileLoader('wxss'),
-          {
-            loader: 'sass-loader',
-            options: {
-              includePaths: [
-                path.resolve('src', 'styles'),
-                path.resolve('src'),
-              ],
-            },
-          },
-        ],
-      }],
-  },
-  plugins: (() => {
-    let webpackPlugins = [
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify(NODE_ENV),
-        'process.env.PLATFORM': JSON.stringify('weChat'),
-      }),
-    ];
-
-    if (IS_PRODUCTION || IS_BETA) {
-      webpackPlugins = [
-        ...webpackPlugins,
-        new CleanWebpackPlugin('./dist', {
-          exclude: ['app.json'],
-          verbose: true,
-          dry: false,
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-          beautify: false,
-          comments: false,
-          compress: {
-              warnings: false,
-              drop_console: true,
-              collapse_vars: true,
-              reduce_vars: true,
-          }
-        }),
-      ]
-    }
-
-    webpackPlugins = [
-      ...webpackPlugins,
-
-      new CopyWebpackPlugin([
-        {from: 'src/app.json', to: path.join(__dirname, 'dist')}
-      ]),
-    ];
-
-    return webpackPlugins;
-  })(),
+    }],
+ },
+ resolve: {
+   modules: ['src', 'node_modules'],
+   extensions: ['.js'],
+ },
 };
-
-module.exports = webpackConfig;
